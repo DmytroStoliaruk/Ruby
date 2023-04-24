198a23cf3a14d77fc905581f5f3a0c573685c0fa
Dmytro Stoliaruk <35365419+DmytroStoliaruk@users.noreply.github.com>
2023-04-24 20:18:42 +0300

    added arrt_reader as posible decision instead of def salary...

--- Task2/Employee.rb
+++ Task2/Employee.rb
@@ -1,6 +1,7 @@
 require_relative("./Person")
 
 class Employee < Person
+  # attr_reader :salary  # second variant instead of def salary...
 
   def initialize (first_name, last_name, birthday, salary)
     super first_name, last_name, birthday
