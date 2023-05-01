5ba5e3715280298e21586951699f72d9c939cb62
Dmytro Stoliaruk <35365419+DmytroStoliaruk@users.noreply.github.com>
2023-05-01 23:50:42 +0300

    OOP Task4

--- Task4/MessageCongratulate.rb
+++ Task4/MessageCongratulate.rb
@@ -2,7 +2,6 @@ class MessageCongratulate < Message
    
   def initialize (str = "day", name = 'everyone')
     str = "Our best wishes, #{name}!!! Happy #{str}!!!"
-    
     super(str)
   end
 
--- Task4/MessageTime.rb
+++ Task4/MessageTime.rb
@@ -3,7 +3,7 @@ require "Date"
 class MessageTime < Message
     
   def initialize (str, time_value = Time.now())
-    str +=  + time_value.strftime("%H:%M")
+    str += time_value.strftime("%H:%M")
     super(str)
   end
   
